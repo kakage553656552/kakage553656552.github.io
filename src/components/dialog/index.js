a1d9fd0e49cea4fae97bdc5c46206688660f1a95
kakage553656552 <Kaka.zhang@mspbots.ai>
2023-11-07 14:55:26 +0800

    Revert "init"
    This reverts commit 5bbb25a3914e9305e099c94e67a291dd84a43f4c.

--- src/components/dialog/index.js
+++
@@ -1,7 +0,0 @@
-import Dialog from '@/components/dialog/index.vue'
-
-Dialog.install = function (Vue) {
-  Vue.component(Dialog.name, Dialog)
-}
-
-export default Dialog
--- src/components/tag/index.js
+++
@@ -1,7 +0,0 @@
-import kaTag from '@/components/tag/index.vue'
-
-kaTag.install = function (Vue) {
-  Vue.component(kaTag.name, kaTag)
-}
-
-export default kaTag
